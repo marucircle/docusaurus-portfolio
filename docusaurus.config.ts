128b01eed388388f23a425fdb00cccbbcfaa00c3
marucircle <2000ryo0113@ezweb.ne.jp>
2024-11-23 02:07:18 +0900

    fix: トップページの修正

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,6 +43,7 @@ const config: Config = {
         showLastUpdateTime: true,
       } satisfies DocsOptions,
     ],
+    'docusaurus-plugin-sass',
   ],
 
   presets: [
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures'
 import Heading from '@theme/Heading'
 
 import styles from './index.module.css'
+import { Profile } from '../components/Top/Profile'
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
@@ -30,8 +31,7 @@ export default function Home(): JSX.Element {
     >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
-        <div>トップページには自分の詳細情報を簡単に書きたい</div>
+        <Profile />
       </main>
     </Layout>
   )
